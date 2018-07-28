a906cddbb39518e4a07b28b760dbff1830c769bf
Pham Van Hai <haivan.qn@gmail.com>
2018-07-28 22:52:28 +0700

    [iSeaTv] Tutorial 2 - Construction of BrowseFragment, Implement HeadersFragment & RowsFragment (GridItemPresenter)

--- app/src/main/java/com/iseasoft/iseatv/MainFragment.java
+++ app/src/main/java/com/iseasoft/iseatv/MainFragment.java
@@ -1,16 +1,84 @@
 package com.iseasoft.iseatv;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.Presenter;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainFragment extends BrowseFragment {
     private static final String TAG = MainFragment.class.getSimpleName();
+    private ArrayObjectAdapter mRowsAdapter;
+    private static final int GRID_ITEM_WIDTH = 300;
+    private static final int GRID_ITEM_HEIGHT = 200;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         Log.i(TAG, "onActivityCreated");
         super.onActivityCreated(savedInstanceState);
+        setupUIElement();
+        loadRows();
 
+    }
+
+    private void setupUIElement(){
+        setBadgeDrawable(getActivity().getResources().getDrawable(R.drawable.banner));
+        //setTitle("Hello Android TV!"); // Badge, when set, takes precedent
+        // over title
+        setHeadersState(HEADERS_ENABLED);
+        setHeadersTransitionOnBackEnabled(true);
+
+        // set fastLane (or headers) background color
+        setBrandColor(getResources().getColor(R.color.fastlane_background));
+        // set search icon color
+        setSearchAffordanceColor(getResources().getColor(R.color.search_opaque));
+    }
+
+    private void loadRows(){
+        mRowsAdapter = new ArrayObjectAdapter(new ListRowPresenter());
+        /* GridItemPresenter */
+        HeaderItem gridItemPresenterHeader = new HeaderItem(0, "GridItemPresenter");
+
+        GridItemPresenter mGridPresenter = new GridItemPresenter();
+        ArrayObjectAdapter gridRowAdapter = new ArrayObjectAdapter(mGridPresenter);
+        gridRowAdapter.add("ITEM 1");
+        gridRowAdapter.add("ITEM 2");
+        gridRowAdapter.add("ITEM 3");
+        mRowsAdapter.add(new ListRow(gridItemPresenterHeader, gridRowAdapter));
+
+        /* set */
+        setAdapter(mRowsAdapter);
+    }
+
+    private class  GridItemPresenter extends Presenter {
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent) {
+            TextView view = new TextView(parent.getContext());
+            view.setLayoutParams(new ViewGroup.LayoutParams(GRID_ITEM_WIDTH, GRID_ITEM_HEIGHT));
+            view.setFocusable(true);
+            view.setFocusableInTouchMode(true);
+            view.setBackgroundColor(getResources().getColor(R.color.default_background));
+            view.setTextColor(Color.WHITE);
+            view.setGravity(Gravity.CENTER);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder viewHolder, Object item) {
+            ((TextView) viewHolder.view).setText((String) item);
+        }
+
+        @Override
+        public void onUnbindViewHolder(ViewHolder viewHolder) {
+
+        }
     }
 }
